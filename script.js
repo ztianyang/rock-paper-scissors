4a6662455ca1b8b2252bae2800078d51215bb894
Tianyang Zhang <tianyangzhang515@gmail.com>
2023-09-15 09:48:07 -0700

    Generate computer choice for rock paper scissors

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
