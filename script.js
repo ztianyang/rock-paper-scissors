671b908244ba8574d339766dd663c3113e9b66d0
Tianyang Zhang <tianyangzhang515@gmail.com>
2023-09-16 21:13:31 -0700

    Implement rock paper scissors game

--- script.js
+++ script.js
@@ -9,8 +9,17 @@ function getComputerChoice() {
     }
 }
 
+function printChoices(playerSelection, computerSelection) {
+    console.log(`You chose: ${playerSelection}`);
+    console.log(`The computer chose: ${computerSelection}`);
+}
+
 function playRound(playerSelection, computerSelection) {
+    if (!playerSelection) {
+        return "invalid";
+    }
     playerSelection = playerSelection.toLowerCase();
+    printChoices(playerSelection, computerSelection);
 
     if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
         return "invalid";
@@ -41,7 +50,37 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose one (rock/paper/scissors): ");
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
 
-console.log(playRound(playerSelection, computerSelection));
+        if (result === "draw") {
+            console.log("It's a draw");
+        } else if (result == "player") {
+            console.log("You score one point!");
+            playerScore++;
+        } else {
+            if (result == "invalid") {
+                console.log("Invalid choice");
+            }
+            console.log("The computer scores one point");
+            computerScore++;
+        }
+    }
+
+    console.log(`Your score: ${playerScore}`);
+    console.log(`Computer score: ${computerScore}`);
+    if (playerScore === computerScore) {
+        console.log("It's a tie!");
+    } else if (playerScore > computerScore) {
+        console.log("You win!");
+    } else {
+        console.log("The computer wins");
+    }
+}
+
+game();
