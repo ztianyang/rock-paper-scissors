76d7d629de710cc8d59d5c9367ddeacb1f633426
Tianyang Zhang <tianyangzhang515@gmail.com>
2023-09-16 20:02:27 -0700

    Add playRound function that determines who wins

--- script.js
+++ script.js
@@ -9,4 +9,35 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    } else {
+        if (playerSelection === "rock") {
+            if (computerSelection === "paper") {
+                return "You lose! Paper beats rock";
+            } else {
+                return "You win! Rock beats scissors";
+            }
+        } else if (playerSelection === "paper") {
+            if (computerSelection === "rock") {
+                return "You win! Paper beats rock";
+            } else {
+                return "You lose! Scissors beat paper";
+            }
+        } else {
+            if (computerSelection === "rock") {
+                return "You lose! Rock beats scissors";
+            } else {
+                return "You win! Scissors beat paper";
+            }
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
